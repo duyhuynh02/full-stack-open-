a63d48d2ec43dfa5cee5153b3ec6da619eb8e024
Duy Huynh <minhduyhuynh16@gmail.com>
2023-09-15 16:30:25 +0700

    done ex 4.5*

--- part 4/bloglist/utils/list_helper.js
+++ part 4/bloglist/utils/list_helper.js
@@ -11,6 +11,32 @@ const totalLikes = (blogs) => {
     
 }
 
+const favoriteBlog = (blogs) => {
+    const first_object = blogs[0]
+
+    const blogsComparing = (first_obj, arr) => {
+        let max_object = first_obj 
+        const res = [first_obj]
+        for (const obj of arr.slice(1)) {
+            if (obj.likes > max_object.likes) {
+                res[0] = obj 
+                max_object = obj 
+            }
+        }
+        return res 
+    }
+
+    const result_array = blogsComparing(first_object, blogs)
+    const result = result_array[0]
+
+
+    return {
+        "title": result.title,
+        "author": result.author, 
+        "likes": result.likes 
+    }
+}
+
 module.exports = {
-    dummy, totalLikes 
+    dummy, totalLikes, favoriteBlog 
 }
