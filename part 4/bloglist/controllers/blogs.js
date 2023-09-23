d4204d48b6ae48d1a27625ab67b9dd5ff93819eb
Duy Huynh <minhduyhuynh16@gmail.com>
2023-09-23 14:57:01 +0700

    done exercise 4.19

--- part 4/bloglist/controllers/blogs.js
+++ part 4/bloglist/controllers/blogs.js
@@ -1,16 +1,30 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user')
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body 
 
-  const user = await User.findById(body.userId)
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token valid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   if (!body.title || !body.url) {
     return response.status(400).end()
--- part 4/bloglist/controllers/login.js
+++ part 4/bloglist/controllers/login.js
@@ -17,7 +17,6 @@ loginRouter.post('/', async (request, response) => {
         })
     }
 
-
     const userForToken = {
         username: user.username, 
         id: user._id, 
