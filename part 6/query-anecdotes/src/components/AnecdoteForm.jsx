485b4b163c9b8b3d936f1c42b55a07ab558498ad
Duy Huynh <minhduyhuynh16@gmail.com>
2023-10-25 15:04:21 +0700

    done exercise 6.23 - well but i'm cannot think of anything to pass the child props back to the parent

--- part 6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part 6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,24 +1,27 @@
 import { createAnecdote } from '../requests'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import Notification from '../components/Notification'
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient()
+  let errorMessage
 
   const newAnecdoteMutation = useMutation({
     mutationFn: createAnecdote, 
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
+    }, 
+    onError: () => {
+      alert('too short anecdote, must have at least 5 or more')
     }
   })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
-    if (content.length >= 5) {
-      event.target.anecdote.value = ''
-      newAnecdoteMutation.mutate({ content, votes: 0 })
-    } 
-}
+    event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0 })
+  } 
 
   return (
     <div>
