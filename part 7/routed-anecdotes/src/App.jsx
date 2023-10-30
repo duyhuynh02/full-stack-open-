3c71a6cff74dac1084a0c71077f76cf4a39bbf4c
Duy Huynh <minhduyhuynh16@gmail.com>
2023-10-30 11:10:42 +0700

    done exercise 7.4

--- part 7/routed-anecdotes/src/App.jsx
+++ part 7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useField } from './hooks'
 import {
   BrowserRouter as Router,
   Routes, Route, Link,
@@ -7,15 +8,15 @@ import {
 } from 'react-router-dom'
 
 const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => 
-                  <li key={anecdote.id} >
-                    <Link to={`/anecdote/${anecdote.id}`}>{anecdote.content}</Link>
-                  </li>)}
-    </ul>
-  </div>
+    <div>
+      <h2>Anecdotes</h2>
+      <ul>
+        {anecdotes.map(anecdote => 
+                    <li key={anecdote.id} >
+                      <Link to={`/anecdote/${anecdote.id}`}>{anecdote.content}</Link>
+                    </li>)}
+      </ul>
+    </div>
 )
 
 const Anecdote = ({ anecdotes }) => {
@@ -53,17 +54,17 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
@@ -75,21 +76,20 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' value={content.value} onChange={content.onChange} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' value={author.value} onChange={author.onChange} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' value={info.value} onChange={info.onChange} />
         </div>
         <button>create</button>
       </form>
     </div>
   )
-
 }
 
 const App = () => {
