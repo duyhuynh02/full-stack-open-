a14f59e9e48298d4c813db6836dda839ed9b0798
Duy Huynh <minhduyhuynh16@gmail.com>
2023-11-18 09:00:55 +0700

    done exercise 8.2

--- part 8/index.js
+++ part 8/index.js
@@ -81,9 +81,18 @@ let books = [
 
 const typeDefs = `
 
+  type Book {
+    title: String!
+    published: Int! 
+    author: String! 
+    id: ID! 
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int! 
+    allBooks: [Book!]!
   }
 
 `
@@ -92,6 +101,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length, 
     authorCount: () => authors.length, 
+    allBooks: () => books,
   }
 }
 
@@ -106,10 +116,3 @@ startStandaloneServer(server, {
   console.log(`Server ready at ${url}`)
 })
 
-//   type Book {
-//     title: String!
-//     published: Int! 
-//     author: String! 
-//     id: ID! 
-//     genres: [String!]!
-//   }
