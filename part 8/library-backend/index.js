522835861b6c7d68944975e85b314cd441df32bc
Duy Huynh <minhduyhuynh16@gmail.com>
2023-12-05 13:36:12 +0700

    fix editAuthor method

--- part 8/library-backend/index.js
+++ part 8/library-backend/index.js
@@ -123,24 +123,23 @@ const resolvers = {
         }
     },
     editAuthor: async (root, args) => {
-      if (args.name.length < 4) {
-        throw new GraphQLError("Author's name should be at least 4 characters", {
-          extensions: { code: 'BAD_USER_INPUT' },
-        });
-      }
-      const author = await Author.findOne({ name: args.name })
+      try {
+        const author = await Author.findOne({ name: args.name })
 
-      if (!author) {
-        return null 
-      }
+        if (!author) {
+          return null 
+        }
 
-      const filter = { _id: author._id }
-      const update = { $set: { born: args.born } }
-      const options = {
-        returnOriginal: false,
-        projection: { _id: 0, name: 1, born: 1, bookCount: 1 }
+        const filter = { _id: author._id }
+        const update = { $set: { born: args.born } }
+        const options = {
+          returnOriginal: false,
+          projection: { _id: 0, name: 1, born: 1, bookCount: 1 }
+        }
+        return Author.findOneAndUpdate(filter, update, options)
+      } catch (error) {
+        throw new Error('Something went wrong while editing the author')
       }
-      return Author.findOneAndUpdate(filter, update, options)
     }
   }
 }
