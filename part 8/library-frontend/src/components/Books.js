37970cbf3e9281a1928ad8f71374ff3619117d06
Duy Huynh <minhduyhuynh16@gmail.com>
2023-12-08 14:46:11 +0700

    fix exercise 8.19

--- part 8/library-frontend/src/components/Books.js
+++ part 8/library-frontend/src/components/Books.js
@@ -9,29 +9,11 @@ const Books = (props) => {
 
   // console.log('props: ', props)
   const books = [...props.books]
-  // console.log('books: ', books)
-
-  // const allGenres = books.map(book => book.genres.map)
-  // console.log(allGenres)
-
-  // const a = books.map(book => book.genres)
-  // const b  = a.map(c => c)
-  // console.log('b: ', b)
-
-  // const allGenres = books.map(book => book.genres).flat()
-  // console.log(allGenres)
-  // const noDuplicateGenres = [...new Set(allGenres)]
-  // console.log('no duplicate genres: ', noDuplicateGenres)
-
   const allNoneDuplicateGenres = [...new Set(books.flatMap(book => book.genres))]
-  // console.log(allNoneDuplicateGenres)
 
   const handleGenre = (event) => {
-    // console.log('event: ', event)
     event.preventDefault()
     setGenre(event.target.textContent)
-    // const genre = event.target.textContent
-    // alert(genre)  
   }
   
   console.log('genre: ', genre)
