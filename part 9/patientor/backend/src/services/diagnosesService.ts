17c0e33cc13de4d49a6691b5351a53cac7089160
Duy Huynh <minhduyhuynh16@gmail.com>
2024-01-02 10:33:24 +0700

    done ex 9.10

---
+++ part 9/patientor/backend/src/services/diagnosesService.ts
@@ -0,0 +1,15 @@
+import diagnosesData from '../data/diagnoses';
+
+import { Diagnosis } from '../types';
+
+const diagnoses: Diagnosis[] = diagnosesData;
+
+const getEntries = (): Diagnosis[] => {
+  return diagnoses;
+};
+
+export default {
+  getEntries,
+};
+
+//hello?
