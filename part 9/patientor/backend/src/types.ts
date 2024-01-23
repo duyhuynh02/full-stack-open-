4060160db80ca44cbf956ffbe88e0d5082cd1b55
Duy Huynh <minhduyhuynh16@gmail.com>
2024-01-23 14:49:17 +0700

    done ex 9.22

--- part 9/patientor/backend/src/types.ts
+++ part 9/patientor/backend/src/types.ts
@@ -35,6 +35,10 @@ interface HospitalEntry extends BaseEntry {
 interface OccupationalHealthcareEntry extends BaseEntry {
     type: "OccupationalHealthcare"; 
     employerName: string; 
+    sickLeave?: {
+        startDate: string; 
+        endDate: string; 
+    };
 }
 
 export type Entry = 
