7e83c32444b86babbe43761e4c40e9e25a5c498c
Duy Huynh <minhduyhuynh16@gmail.com>
2024-03-01 13:31:46 +0700

    Done ex 10.5

--- part 10/rate-repository-app/src/components/RepositoryItem.jsx
+++ part 10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,15 +1,80 @@
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
+import theme from '../theme';
+import format from '../helper';
+
+const styles = StyleSheet.create({
+    flexContainer: {
+        display: 'flex',
+    },
+    flexFirstRow: {
+        display: 'flex',
+        flexDirection: 'row', 
+        flexGrow: 0,
+    },
+    flexSecondRow: {
+        display: 'flex',
+        flexGrow: 1, 
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+    }, 
+    flexThirdRow: {
+        display: 'flex',
+    },
+    flexSecondColumn: {
+        paddingLeft: 12.5,
+    },
+    logo: {
+        width: 56, 
+        height: 58,
+    },
+    text: {
+        fontSize: theme.fontSizes.body,
+        fontWeight: theme.fontWeights.bold, 
+    },
+    tag: {
+        color: theme.colors.white,
+        fontWeight: theme.fontWeights.bold, 
+        backgroundColor: theme.colors.primary, 
+        alignSelf: 'flex-start',
+        borderRadius: 5, 
+    }
+});
+
 
 const RepositoryItem = ({ item }) => {
     return (
-        <View>
-            <Text>Full Name: {item.fullName}</Text>
-            <Text>Description: {item.description}</Text>
-            <Text>Language: {item.language}</Text>
-            <Text>Forks: {item.forksCount}</Text>
-            <Text>Stars {item.stargazersCount}</Text>
-            <Text>Rating: {item.ratingAverage}</Text>
-            <Text>Reviews: {item.reviewCount}</Text>
+        <View style={styles.flexContainer}>
+            <View style={styles.flexFirstRow}>
+                <Image 
+                    style={styles.logo}
+                    source={{uri: item.ownerAvatarUrl}}
+                />
+                <View style={styles.flexSecondColumn}>
+                    <Text style={styles.text}>{item.fullName}</Text>
+                    <Text>{item.description}</Text>
+                    <Text style={styles.tag}>{item.language}</Text>
+                </View>
+            </View>
+
+            <View style={styles.flexSecondRow}>
+                <View style={styles.flexThirdRow}>
+                    <Text style={styles.text}>{format.format(item.forksCount)}</Text>
+                    <Text>Forks</Text>
+                </View>  
+                <View style={styles.flexThirdRow}>
+                    <Text style={styles.text}>{format.format(item.stargazersCount)}</Text>
+                    <Text>Counts</Text>
+                </View>  
+                <View style={styles.flexThirdRow}>
+                    <Text style={styles.text}>{format.format(item.ratingAverage)}</Text>
+                    <Text>Average</Text>
+                </View>  
+                <View style={styles.flexThirdRow}>
+                    <Text style={styles.text}>{format.format(item.reviewCount)}</Text>
+                    <Text>Reviews</Text>
+                </View>  
+            </View>
+
         </View>
     );
 };
--- part 10/rate-repository-app/src/theme.js
+++ part 10/rate-repository-app/src/theme.js
@@ -3,6 +3,8 @@ const theme = {
       textPrimary: '#24292e',
       textSecondary: '#586069',
       primary: '#0366d6',
+      white: 'white',
+      main: '#e1e4e8'
     },
     fontSizes: {
       body: 14,
