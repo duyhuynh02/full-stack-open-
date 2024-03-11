1d8cad0b96865e881b9cf50483da2d1da2cb6d5c
Duy Huynh <minhduyhuynh16@gmail.com>
2024-03-11 11:30:31 +0700

    done ex 10.7

--- part 10/rate-repository-app/src/components/AppBar.jsx
+++ part 10/rate-repository-app/src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text, Pressable } from 'react-native';
+import { View, StyleSheet, Text, Pressable, ScrollView } from 'react-native';
 import { Link } from "react-router-native";
 import Constants from 'expo-constants';
 import theme from '../theme';
@@ -6,23 +6,31 @@ import theme from '../theme';
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // paddingHorizontal: 16,
   },
   text: {
     fontSize: theme.fontSizes.heading,
     fontWeight: theme.fontWeights.bold, 
     color: "white",
     backgroundColor: '#24292e',
+  }, 
+  scrollView: {
+    marginHorizontal: 20, 
   }
 });
 
 const AppBar = () => {
   return <View style={styles.container}>
+      <ScrollView horizontal style={styles.scrollView}>
         <Pressable onPress={() => {}}>
             <Text style={styles.text}>Repositories</Text>
             <Link to="/login">
               <Text>Sign In</Text>
             </Link>
         </Pressable>
+      </ScrollView>
     </View>;
 };
 
