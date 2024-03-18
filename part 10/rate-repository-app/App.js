165fe0388f76f8cafbac167834d51dbe7761be7e
Duy Huynh <minhduyhuynh16@gmail.com>
2024-03-18 19:30:02 +0700

    preparation for ex 10.12

--- part 10/rate-repository-app/App.js
+++ part 10/rate-repository-app/App.js
@@ -2,10 +2,12 @@ import Main from './src/components/Main';
 import { ApolloProvider } from '@apollo/client';
 import { NativeRouter } from 'react-router-native';
 import createApolloClient from './src/utils/apolloClient';
+// import Constants from 'expo-constants'; 
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+
   return (
     <>
       <NativeRouter>
